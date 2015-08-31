b37e0c8c2594d6384e83bade44a875ecfd31197d
Jakub Wilk <jwilk@jwilk.net>
2015-08-31 14:15:34 +0200

    tests/test_persistent: add.

---
+++ tests/test_persistent.py
@@ -0,0 +1,80 @@
+# encoding=UTF-8
+
+# Copyright © 2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import signal
+
+from nose.tools import assert_equal
+from . import tools
+
+import afl
+
+def test_non_persistent():
+    os.environ.pop('AFL_PERSISTENT', None)
+    _test_non_persistent()
+    _test_non_persistent(1)
+    _test_non_persistent(max=1)
+    _test_non_persistent(42)
+    _test_non_persistent(max=42)
+
+@tools.fork_isolation
+def _test_non_persistent(*args, **kwargs):
+    assert os.getenv('AFL_PERSISTENT') is None
+    x = 0
+    while afl.persistent(*args, **kwargs):
+        x += 1
+    assert_equal(x, 1)
+
+def test_persistent():
+    try:
+        os.environ['AFL_PERSISTENT'] = '1'
+        _test_persistent(None)
+        _test_persistent(1, 1)
+        _test_persistent(1, max=1)
+        _test_persistent(42, 42)
+        _test_persistent(42, max=42)
+    finally:
+        os.environ.pop('AFL_PERSISTENT', None)
+
+@tools.fork_isolation
+def _test_persistent(n, *args, **kwargs):
+    assert os.getenv('AFL_PERSISTENT') is not None
+    n_max = 1000
+    k = [0]
+    def kill(pid, sig):
+        assert_equal(pid, os.getpid())
+        assert_equal(sig, signal.SIGSTOP)
+        k[0] += 1
+    os.kill = kill
+    x = 0
+    while afl.persistent(*args, **kwargs):
+        x += 1
+        if x == n_max:
+            break
+    if n is None:
+        assert_equal(x, n_max)
+        assert_equal(k[0], n_max - 1)
+    else:
+        assert_equal(x, n)
+        assert_equal(k[0], n)
+
+# vim:ts=4 sts=4 sw=4 et
---
+++ tests/tools.py
@@ -0,0 +1,92 @@
+# encoding=UTF-8
+
+# Copyright © 2013-2015 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import functools
+import traceback
+import sys
+import os
+
+import nose
+
+class IsolatedError(Exception):
+    pass
+
+def _n_relevant_tb_levels(tb):
+    n = 0
+    while tb and '__unittest' not in tb.tb_frame.f_globals:
+        n += 1
+        tb = tb.tb_next
+    return n
+
+def fork_isolation(f):
+
+    EXIT_EXCEPTION = 101
+    EXIT_SKIP_TEST = 102
+
+    exit = os._exit
+    # sys.exit() can't be used here, because nose catches all exceptions,
+    # including SystemExit
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        readfd, writefd = os.pipe()
+        pid = os.fork()
+        if pid == 0:
+            # child:
+            os.close(readfd)
+            try:
+                f(*args, **kwargs)
+            except nose.SkipTest as exc:
+                s = str(exc).encode('UTF-8')
+                with os.fdopen(writefd, 'wb') as fp:
+                    fp.write(s)
+                exit(EXIT_SKIP_TEST)
+            except Exception:
+                exctp, exc, tb = sys.exc_info()
+                s = traceback.format_exception(exctp, exc, tb, _n_relevant_tb_levels(tb))
+                s = ''.join(s).encode('UTF-8')
+                del tb
+                with os.fdopen(writefd, 'wb') as fp:
+                    fp.write(s)
+                exit(EXIT_EXCEPTION)
+            exit(0)
+        else:
+            # parent:
+            os.close(writefd)
+            with os.fdopen(readfd, 'rb') as fp:
+                msg = fp.read()
+            msg = msg.decode('UTF-8').rstrip('\n')
+            pid, status = os.waitpid(pid, 0)
+            if status == (EXIT_EXCEPTION << 8):
+                raise IsolatedError('\n\n' + msg)
+            elif status == (EXIT_SKIP_TEST << 8):
+                raise nose.SkipTest(msg)
+            elif status == 0 and msg == '':
+                pass
+            else:
+                raise RuntimeError('unexpected isolated process status {}'.format(status))
+
+    return wrapper
+
+__all__ = ['fork_isolation']
+
+# vim:ts=4 sts=4 sw=4 et
