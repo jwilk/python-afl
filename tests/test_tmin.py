03f4b20304c5474065d6e4daff0fbbe848d366db
Jakub Wilk <jwilk@jwilk.net>
2017-04-03 23:06:33 +0200

    tests/test_tmin: add.

---
+++ tests/test_tmin.py
@@ -0,0 +1,73 @@
+# encoding=UTF-8
+
+# Copyright © 2015-2017 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import subprocess as ipc
+import sys
+
+from .tools import (
+    assert_equal,
+    clean_environ,
+    tempdir,
+)
+
+here = os.path.dirname(__file__)
+target = here + '/target.py'
+
+def run(cmd, stdin='', xstatus=0):
+    child = ipc.Popen(
+        list(cmd),
+        stdin=ipc.PIPE,
+        stdout=ipc.PIPE,
+        stderr=ipc.PIPE,
+        preexec_fn=clean_environ,
+    )
+    (stdout, stderr) = child.communicate(stdin)
+    if child.returncode != xstatus:
+        if str is not bytes:
+            stderr = stderr.decode('ASCII', 'replace')
+        print(stderr)
+        raise ipc.CalledProcessError(child.returncode, cmd[0])
+    return (stdout, stderr)
+
+def run_afl_tmin(input, xoutput, xstatus=0):
+    with tempdir() as workdir:
+        inpath = workdir + '/in'
+        with open(inpath, 'wb') as file:
+            file.write(input)
+        outpath = workdir + '/out'
+        run(
+            ['py-afl-tmin', '-i', inpath, '-o', outpath, '--', sys.executable, target],
+            xstatus=xstatus,
+        )
+        with open(outpath, 'rb') as file:
+            output = file.read()
+        assert_equal(output, xoutput)
+
+def test():
+    run_afl_tmin(b'0' * 6, b'0')
+    run_afl_tmin(b'X' * 7, b'X')
+
+def test_exc():
+    run_afl_tmin(b'\xcf\x87', b'\x87')
+
+# vim:ts=4 sts=4 sw=4 et
