jwilk/python-afl

commit 5726d71f64f80f71af7049eec7cf7f619db659d8

setup: simplify wheel support code.

setup.py
@@ -118,16 +118,13 @@ class cmd_sdist(distutils_sdist):
         distutils_sdist.make_release_tree(self, base_dir, files)
         self.maybe_move_file(base_dir, 'LICENSE', 'doc/LICENSE')
 
-cmdclass = dict(
-    sdist=cmd_sdist,
-)
-if bdist_wheel is not None:
-    cmdclass.update(bdist_wheel=bdist_wheel)
-
 distutils.core.setup(
     ext_modules=Cython.Build.cythonize('afl.pyx'),
     scripts=glob.glob('py-afl-*'),
-    cmdclass=cmdclass,
+    cmdclass=dict(
+        bdist_wheel=bdist_wheel,
+        sdist=cmd_sdist,
+    ),
     **meta
 )
 

commit 80bb9432f80e48b96244375146a69ff792daa1d2

setup: don't pass None to cmdclass.
Fixes: 5726d71f64f80f71af7049eec7cf7f619db659d8

Fixes:

    $ python setup.py --help-commands
    ...
    Extra commands:
    Traceback (most recent call last):
      File "setup.py", line 128, in <module>
        **meta
      File "/usr/lib/python2.7/distutils/core.py", line 137, in setup
        ok = dist.parse_command_line()
      File "/usr/lib/python2.7/distutils/dist.py", line 464, in parse_command_line
        if self.handle_display_options(option_order):
      File "/usr/lib/python2.7/distutils/dist.py", line 669, in handle_display_options
        self.print_commands()
      File "/usr/lib/python2.7/distutils/dist.py", line 745, in print_commands
        max_length)
      File "/usr/lib/python2.7/distutils/dist.py", line 706, in print_command_list
        klass = self.get_command_class(cmd)
      File "/usr/lib/python2.7/distutils/dist.py", line 830, in get_command_class
        raise DistutilsModuleError("invalid command '%s'" % command)
    distutils.errors.DistutilsModuleError: invalid command 'bdist_wheel'

setup.py
@@ -118,10 +118,16 @@ class cmd_sdist(distutils_sdist):
         distutils_sdist.make_release_tree(self, base_dir, files)
         self.maybe_move_file(base_dir, 'LICENSE', 'doc/LICENSE')
 
+def d(**kwargs):
+    return dict(
+        (k, v) for k, v in kwargs.items()
+        if v is not None
+    )
+
 distutils.core.setup(
     ext_modules=Cython.Build.cythonize('afl.pyx'),
     scripts=glob.glob('py-afl-*'),
-    cmdclass=dict(
+    cmdclass=d(
         bdist_wheel=bdist_wheel,
         sdist=cmd_sdist,
     ),