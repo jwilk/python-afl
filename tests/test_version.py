e4cae5909beb3040cad78f7f9d6546ed9d116edf
Jakub Wilk <jwilk@jwilk.net>
2019-03-22 22:14:54 +0100

    tests/test_version: update copyright notice.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2015-2017 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2015-2019 Jakub Wilk <jwilk@jwilk.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the “Software”), to deal
