9b3238b4be0ce9197978304852aa7297225e4af5
Jakub Wilk <jwilk@jwilk.net>
2021-03-04 16:00:13 +0100

    Bump version to 0.7.4.

--- afl.pyx
+++ afl.pyx
@@ -27,7 +27,7 @@
 American Fuzzy Lop fork server and instrumentation for pure-Python code
 '''
 
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 
 cdef object os, signal, struct, sys, warnings
 import os
