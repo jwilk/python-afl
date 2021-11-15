d14e1329de504bcf374ff857e4d46388fcccd2b9
Jakub Wilk <jwilk@jwilk.net>
2021-11-15 20:27:51 +0100

    tests.tools: stop using nose.
    This breaks compatibility with Python 2.6. Oh well.

--- tests/tools.py
+++ tests/tools.py
@@ -29,93 +29,59 @@ import subprocess as ipc
 import sys
 import tempfile
 import traceback
+import unittest
 import warnings
 
-import nose.tools
-
-from nose import SkipTest
-
-from nose.tools import (
-    assert_equal,
-    assert_not_equal,
-    assert_true,
-)
+SkipTest = unittest.SkipTest
 
 def assert_fail(msg):
     assert_true(False, msg=msg)  # pylint: disable=redundant-unittest-assert
 
-def noseimport(vmaj, vmin, name=None):
-    def wrapper(f):
-        if f.__module__ == 'unittest.case':
-            return f
-        if sys.version_info >= (vmaj, vmin):
-            return getattr(nose.tools, name or f.__name__)
-        return f
-    return wrapper
+tc = unittest.TestCase('__hash__')
 
-@noseimport(2, 7)
-class assert_raises(object):
-    def __init__(self, exc_type):
-        self._exc_type = exc_type
-        self.exception = None
-    def __enter__(self):
-        return self
-    def __exit__(self, exc_type, exc_value, tb):
-        if exc_type is None:
-            assert_fail('{0} not raised'.format(self._exc_type.__name__))
-        if not issubclass(exc_type, self._exc_type):
-            return False
-        if isinstance(exc_value, exc_type):
-            pass
-            # This branch is not always taken in Python 2.6:
-            # https://bugs.python.org/issue7853
-        elif isinstance(exc_value, tuple):
-            exc_value = exc_type(*exc_value)
-        else:
-            exc_value = exc_type(exc_value)
-        self.exception = exc_value
-        return True
+assert_equal = tc.assertEqual
 
-@noseimport(2, 7, 'assert_raises_regexp')
-@noseimport(3, 2)
-@contextlib.contextmanager
-def assert_raises_regex(exc_type, regex):
-    with assert_raises(exc_type) as ecm:
-        yield
-    assert_regex(str(ecm.exception), regex)
+assert_not_equal = tc.assertNotEqual
 
-@noseimport(2, 7, 'assert_regexp_matches')
-@noseimport(3, 2)
-def assert_regex(text, regex):
-    try:
-        str_types = basestring
-    except NameError:
-        str_types = (str, bytes)
-    if isinstance(regex, str_types):
-        regex = re.compile(regex)
-    if not regex.search(text):
-        message = "Regex didn't match: {0!r} not found in {1!r}".format(regex.pattern, text)
-        assert_fail(msg=message)
+assert_true = tc.assertTrue
 
-@noseimport(3, 2)
-@contextlib.contextmanager
-def assert_warns_regex(exc_type, regex):
-    with warnings.catch_warnings(record=True) as wlog:
-        warnings.simplefilter('always', exc_type)
-        yield
-    firstw = None
-    for warning in wlog:
-        w = warning.message
-        if not isinstance(w, exc_type):
-            continue
+assert_raises = tc.assertRaises
+
+# pylint: disable=no-member
+if sys.version_info >= (3,):
+    assert_raises_regex = tc.assertRaisesRegex
+else:
+    assert_raises_regex = tc.assertRaisesRegexp
+# pylint: enable=no-member
+
+# pylint: disable=no-member
+if sys.version_info >= (3,):
+    assert_regex = tc.assertRegex
+else:
+    assert_regex = tc.assertRegexpMatches
+# pylint: enable=no-member
+
+if sys.version_info >= (3,):
+    assert_warns_regex = tc.assertWarnsRegex  # pylint: disable=no-member
+else:
+    @contextlib.contextmanager
+    def assert_warns_regex(exc_type, regex):
+        with warnings.catch_warnings(record=True) as wlog:
+            warnings.simplefilter('always', exc_type)
+            yield
+        firstw = None
+        for warning in wlog:
+            w = warning.message
+            if not isinstance(w, exc_type):
+                continue
+            if firstw is None:
+                firstw = w
+            if re.search(regex, str(w)):
+                return
         if firstw is None:
-            firstw = w
-        if re.search(regex, str(w)):
-            return
-    if firstw is None:
-        assert_fail(msg='{exc} not triggered'.format(exc=exc_type.__name__))
-    else:
-        assert_fail(msg='{exc!r} does not match {re!r}'.format(exc=str(firstw), re=regex))
+            assert_fail(msg='{exc} not triggered'.format(exc=exc_type.__name__))
+        else:
+            assert_fail(msg='{exc!r} does not match {re!r}'.format(exc=str(firstw), re=regex))
 
 class IsolatedException(Exception):
     pass
@@ -177,8 +143,8 @@ def fork_isolation(f):
     EXIT_SKIP_TEST = 102
 
     exit = os._exit  # pylint: disable=redefined-builtin,protected-access
-    # sys.exit() can't be used here, because nose catches all exceptions,
-    # including SystemExit
+    # sys.exit() can't be used here, because the test harness
+    # catches all exceptions, including SystemExit
 
     # pylint:disable=consider-using-sys-exit
 
